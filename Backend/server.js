10b532061b547a0e0f227c53818ab1acfa0bd07d
Hiran-sudheer <gethiranonline@gmail.com>
2024-12-23 00:47:19 +0530

    Completed Backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,17 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connetToMongoDB from "./db/connectToMongoDb.js";
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-
-
-app.use(express.json()); 
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 // app.get("/", (req, res) => {
 //     // root route http://localhost:5000/
